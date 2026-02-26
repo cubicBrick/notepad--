e79484a28aae76b714f8cbb6a4ef301e2f611002
cubicBrick <jaydenhor@outlook.com>
2026-02-25 19:58:27 -0800

    attack shark has code

---
+++ linux/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.2.3 (trial), 000000, 2026-02-25T19:42:43.714179
+from .pyarmor_runtime import __pyarmor__
---
+++ windows/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.2.3 (trial), 000000, 2026-02-25T19:05:13.278006
+from .pyarmor_runtime import __pyarmor__
